90149b34e72e0707a6a658c18489603d846bb61a
ShenSouza <ds052798ts@gmail.com>
2021-03-02 20:00:49 -0300

    chore(security): Implementando a camada de Security e configurando #10

---
+++ blogapi/src/main/java/com/blogapi/blogapi/security/BasicSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.blogapi.blogapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class BasicSecurityConfig {
+
+	
+}
