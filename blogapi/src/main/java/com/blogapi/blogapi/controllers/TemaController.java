822ead286c7d54073335fe44f2dd211a51071f6c
ShenSouza <ds052798ts@gmail.com>
2021-03-02 19:17:28 -0300

    feat(controller): Criado delete da TemaController #8

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,10 @@ public class TemaController {
 	@PutMapping
 	public ResponseEntity<Tema> put(@RequestBody Tema tema) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
