6017b57a5a5ca2f194c1dc63d68b5e0d9ee377a9
ShenSouza <ds052798ts@gmail.com>
2021-03-02 20:24:53 -0300

    chore(models): Criado a UserDetailsServiceImpl #20

---
+++ blogapi/src/main/java/com/blogapi/blogapi/security/UserDetailsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.blogapi.blogapi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+}
