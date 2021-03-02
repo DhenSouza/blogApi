8011c545d8cfd73d2230031ff4d73249f47d8078
ShenSouza <ds052798ts@gmail.com>
2021-03-02 18:07:17 -0300

    feat(controller): Criado findById da classe Tema #5

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
@@ -1,17 +1,33 @@
 package com.blogapi.blogapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blogapi.blogapi.models.Tema;
 import com.blogapi.blogapi.repositories.TemaRepository;
 
 @RestController
-@RequestMapping("/tema")
+@RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TemaController {
 
 	@Autowired
 	private TemaRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Tema>> findAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> findById(@PathVariable long id) {
+		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
+	}
 }
