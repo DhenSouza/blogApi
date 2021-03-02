2dcfe9c6c8681d72afa261a7fa3fdf241f63c4da
ShenSouza <ds052798ts@gmail.com>
2021-03-02 19:12:53 -0300

    feat(controller): Criado método Post da TemaController #6

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/TemaController.java
@@ -3,10 +3,13 @@ package com.blogapi.blogapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,10 +33,15 @@ public class TemaController {
 	public ResponseEntity<Tema> findById(@PathVariable long id) {
 		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	@GetMapping("nome/{nome}")
-	public ResponseEntity<List<Tema>> findByNome (@PathVariable String nome){
+	public ResponseEntity<List<Tema>> findByNome(@PathVariable String nome) {
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Tema> post(@RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	}
+
 }
