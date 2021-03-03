c0bc9679ad6993a412d12616385b03c784ad0ed8
ShenSouza <ds052798ts@gmail.com>
2021-03-02 21:29:45 -0300

    feat(controllers): Criado Método de Login Usuario #19

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/UsuarioController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/UsuarioController.java
@@ -1,12 +1,17 @@
 package com.blogapi.blogapi.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.blogapi.blogapi.models.UserLogin;
 import com.blogapi.blogapi.services.UsuarioService;
 
 @RestController
@@ -17,4 +22,9 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.logar(user).map(obj -> ResponseEntity.ok(obj))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
 }
