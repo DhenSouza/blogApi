207d05d5735e9289623cf28acde4d80cda1cbcc8
ShenSouza <ds052798ts@gmail.com>
2021-03-03 14:09:54 -0300

    feat(controllers): Implementado método post postagens #27

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/PostagensController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/PostagensController.java
@@ -3,10 +3,13 @@ package com.blogapi.blogapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +38,10 @@ public class PostagensController {
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagens>> findByTitulo (@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagens> post(@RequestBody Postagens postagens) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagens));
 	}
 }
