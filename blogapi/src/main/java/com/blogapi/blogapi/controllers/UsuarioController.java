b11afde6c6cf3b84592bf80ccef8179f90ba3ca0
ShenSouza <ds052798ts@gmail.com>
2021-03-02 21:26:17 -0300

    chore(controllers): Criado a classe UsuarioController #17

---
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.blogapi.blogapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blogapi.blogapi.services.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+}
