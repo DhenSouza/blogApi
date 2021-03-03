87bf80eaaae2b3e00740b9c0af0314bdffd96f51
ShenSouza <ds052798ts@gmail.com>
2021-03-03 13:18:28 -0300

    chore(models): Inserido Model de postagens #21

---
+++ blogapi/src/main/java/com/blogapi/blogapi/models/Postagens.java
@@ -0,0 +1,47 @@
+package com.blogapi.blogapi.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "postagens")
+public class Postagens {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String titulo;
+
+	@NotNull(message = "O Campo TEXTO não pode estar vazio")
+	private String texto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
