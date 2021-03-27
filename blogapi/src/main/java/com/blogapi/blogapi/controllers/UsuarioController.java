55efa7e5403618d6640c3456d7a7ce2faa49f9f2
ShenSouza <ds052798ts@gmail.com>
2021-03-27 19:15:47 -0300

    feat(UsuarioController): Inserido o getById no UsuarioController #38

--- blogapi/src/main/java/com/blogapi/blogapi/controllers/UsuarioController.java
+++ blogapi/src/main/java/com/blogapi/blogapi/controllers/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.blogapi.blogapi.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.blogapi.blogapi.models.UserLogin;
 import com.blogapi.blogapi.models.Usuario;
+import com.blogapi.blogapi.repositories.UsuarioRepository;
 import com.blogapi.blogapi.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.logar(user).map(obj -> ResponseEntity.ok(obj))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id ) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping("/cadastrar")
